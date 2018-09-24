f17cd2c3d9eb57d71fa37db1cb9c559205629433
centos7 <1021922388@qq.com>
2018-09-25 02:03:58 +0800

    re

--- src/bean/YHConstants.java
+++
@@ -1,9 +0,0 @@
-package bean;
-
-
-public class YHConstants {  
-	    public static final int LENGTH = 7;// 命令数据类型  
-	    public static final String YH_CMD = "YH CMD ";// 命令数据类型  
-	    public static final String YH_IMG = "YH IMG ";// 图片数据类型  
-	
-}
